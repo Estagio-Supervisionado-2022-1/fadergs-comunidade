ffccb929ee6654be0a69e511d9167234581fc7f1
ThierrryScotto <thierry_192@outlook.com>
2022-04-21 16:17:24 -0300

    --append

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,7 +77,7 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         $user->update([
-            "name" => $request->nome,
+            "name" => $request->name,
             "email" => $request->email,
             "password" => Crypt::encrypt($request->password)
         ]);
